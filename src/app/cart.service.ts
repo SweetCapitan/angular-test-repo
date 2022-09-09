3c79e4b01a9ec1dc7f29a10c4c62658c635268f9
SweetCapitan <danil.duhanin@yandex.ru>
2022-09-10 01:34:04 +0300

    Add simply cart, shipping prices service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  items: Product = [];
+  items: Product[] = [];
 
   addToCart(product: Product) {
     this.items.push(product);
   }
-  constructor() { }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+  
+  constructor(private http: HttpClient) { }
 }
